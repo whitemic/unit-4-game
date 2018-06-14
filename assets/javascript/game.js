8d0a2bc20cc61fd408c6d9bce530eb2083712c0b
Michaela White <whitemic@sas.upenn.edu>
2018-06-14 14:00:46 -0400

    Added game result label

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,5 @@
 $(document).ready(function(){
-
-    var crystal1Val = 0;
-    var crystal2Val = 0;
-    var crystal3Val = 0;
-    var crystal4Val = 0;
     var gameNumber = 0;
-
     var currentScore = 0;
     var winsCounter = 0;
     var lossCounter = 0;
@@ -13,20 +7,17 @@ $(document).ready(function(){
     var gameLost = false;
 
     function restart() {
-        crystal1Val = Math.floor((Math.random() * 12) + 1);
-        crystal2Val = Math.floor((Math.random() * 12) + 1);
-        crystal3Val = Math.floor((Math.random() * 12) + 1); 
-        crystal4Val = Math.floor((Math.random() * 12) + 1);
+        $(".button").attr("value", "0");
+        $("#win-lose").text("");
         gameNumber = Math.floor((Math.random() * 101) + 19);
-    
         currentScore = 0;
         gameWon = false;
         gameLost = false;
-
         update();
     }
 
     function update() {
+        $("#instructions").text("How to Play: Choose and click on any of the crystal buttons. Each one will be worth a random value between one and twelve for each game you play. When you click on a crystal you will see your current score increase by the value of the crystal you chose. Your goal is to use the crystals strategically so that your current score matches the designated game score for that round. If you go over this amount, you will lose the game.");
         $("#number").text(gameNumber);
         $("#number2").text(currentScore);
         $("#outcome-box").text("Wins: " + winsCounter + " Losses: " + lossCounter);
@@ -46,11 +37,17 @@ $(document).ready(function(){
 
     function winGame() {
         winsCounter++;
+        $("#win-lose").text("You won!");
         update();
+        setTimeout(restart, 1000);
+        // restart();
     }
     function loseGame() {
         lossCounter++;
+        $("#win-lose").text("You lost!");
         update();
+        setTimeout(restart, 1000);
+        // restart();
     }
 
 
@@ -58,83 +55,24 @@ $(document).ready(function(){
 restart();
 update(); 
 
-
-    $("#button1").on("click", function() {
-        currentScore += crystal1Val;
-        update();
-        checkWon();
-        checkLost();
-        if (gameWon) {
-            //flash something saying you won
-            winGame();
-            restart();
-        } else {
-            if (gameLost) {
-                //flash something saying you lost
-                loseGame();
-                restart();
-            }
+$(".button").on("click", function() {
+    var value1 = $(this).attr("value");
+    var value2 = parseInt(value1);
+    if (value2 === 0) {
+        $(this).attr("value", Math.floor(Math.random() * 12) + 1);
+    }
+    currentScore += parseInt($(this).attr("value"));
+    update();
+    checkWon();
+    checkLost();
+    if (gameWon) {
+        winGame();
+        // restart();
+    } else {
+        if (gameLost) {
+            loseGame();
+            // restart();
         }
-        console.log(crystal1Val);
-    });
-
-    $("#button2").on("click", function() {
-        currentScore += crystal2Val;
-        update();
-        checkWon();
-        checkLost();
-        if (gameWon) {
-            //flash something saying you won
-            winGame();
-            restart();
-        } else {
-            if (gameLost) {
-                //flash something saying you lost
-                loseGame();
-                restart();
-            }
-        }
-        
-        console.log(crystal2Val);
-    });
-
-    $("#button3").on("click", function() {
-        currentScore += crystal3Val;
-        update();
-        checkWon();
-        checkLost();
-        if (gameWon) {
-            //flash something saying you won
-            winGame();
-            restart();
-        } else {
-            if (gameLost) {
-                //flash something saying you lost
-                loseGame();
-                restart();
-            }
-        }
-        
-        console.log(crystal3Val);
-    });
-
-    $("#button4").on("click", function() {
-        currentScore += crystal4Val;
-        update();
-        checkWon();
-        checkLost();
-        if (gameWon) {
-            //flash something saying you won
-            winGame();
-            restart();
-        } else {
-            if (gameLost) {
-                //flash something saying you lost
-                loseGame();
-                restart();
-            }
-        }
-        console.log(crystal4Val);
-    });
-
+    }
+})
 })
